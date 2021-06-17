745224dd92c8a04f6d6f7775d2d673e20ba3d681
BenHammondMusic <41567007+benhammondmusic@users.noreply.github.com>
2021-06-17 15:27:24 -0600

    csharp hello world

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello c# World!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
